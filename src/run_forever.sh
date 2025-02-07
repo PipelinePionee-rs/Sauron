5b7d2b0c4f303d6cf794c0ccde3d522ede525ee9
madsfjeldberg <mafj0001@stud.kea.dk>
2025-02-07 12:35:49 +0100

    updated shell script with shellcheck

--- src/run_forever.sh
+++ src/run_forever.sh
@@ -2,13 +2,11 @@
 
 PYTHON_SCRIPT_PATH=$1
 
-TMP="This variable might become useful at some point. Otherwise delete it." 
-
 while true
 do
-    python2 $PYTHON_SCRIPT_PATH
-    if [ $? -ne 0 ]; then
-        echo "Script crashed with exit code $?. Restarting..." >&2
+    if ! python3 "$PYTHON_SCRIPT_PATH"; then
+    exit_code=$?
+        echo "Script crashed with exit code $exit_code. Restarting..." >&2
         sleep 1
     fi
 done
