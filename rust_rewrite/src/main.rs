70c53cca43a2b0711e7b42ded75e7e38b568ff8f
madsfjeldberg <mafj0001@stud.kea.dk>
2025-02-11 16:30:45 +0100

    added more dependencies w. comments, and boilerplate axum code.

--- rust_rewrite/src/main.rs
+++ rust_rewrite/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use axum::response::Html;
+use axum::routing::get;
+use axum::Router;
+
+
+#[tokio::main]
+async fn main() {
+  println!("The Eye of Sauron is opening...");
+
+  let routes_hello = Router::new().route(
+    "/hello",
+    get(|| async { Html("Hello Sauron!")}),
+  );
+
+	let listener = TcpListener::bind("localhost:8080").await.unwrap();
+	println!("->> LISTENING on {:?}\n", listener.local_addr());
+	axum::serve(listener, routes_hello.into_make_service())
+		.await
+		.unwrap();
 }
