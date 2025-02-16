536029e77ba9dbdc936c35509bb9cbee3d7de9dd
Lars <144362461+laha0006@users.noreply.github.com>
2025-02-16 16:18:57 +0100

    implemented create_Db_connection function

--- rust_rewrite/src/db.rs
+++ rust_rewrite/src/db.rs
@@ -1,2 +1,7 @@
 use tokio_rusqlite::{Connection, Result};
 use rusqlite::params;
+
+pub async fn create_db_connection() -> Result<Connection> {
+    let conn = Connection::open("sauron.db").await?;
+    Ok(conn)
+}
