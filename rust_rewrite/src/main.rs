81e4b62d78e8d1405344d7aa6da590438cf9c762
Lars <144362461+laha0006@users.noreply.github.com>
2025-02-19 21:31:11 +0100

    added static routes to paths of openapi spec

--- rust_rewrite/src/main.rs
+++ rust_rewrite/src/main.rs
@@ -29,7 +29,7 @@ use crate::db::create_db_connection;
 // so if we want to add more paths, we just do #[openapi(paths(path1, path2, path3))]
 #[derive(OpenApi)] // this attribute derives the OpenApi impl for the struct
 #[openapi(
-  paths(hello, api::api_search, api::api_login, api::api_register, api::api_logout, api::root_handler)
+  paths(hello, api::api_search, api::api_login, api::api_register, api::api_logout, api::root_dummy, api::search_dummy)
 )] // this attribute specifies the paths that will be documented
 // structs are like classes in Java, but without methods
 struct ApiDoc; // this is the struct that will be used to generate the OpenAPI documentation
