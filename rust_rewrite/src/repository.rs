880277050aa6206abea096d5eeb06ea74b89e5f2
MuLangballe <mala0007@stud.kea.dk>
2025-02-25 14:50:59 +0100

    Deleted comments/pseudocode

--- rust_rewrite/src/repository.rs
+++ rust_rewrite/src/repository.rs
@@ -44,10 +44,4 @@ impl PageRepository {
     }
 }
 
-// TODO: lav 1 assosiated function new() som er en slags construktor den skal nok tage en path til DB filen
-    //så du kan lave en connetion. bliver kaldt ved at sige pageRepository::new()
-    // TODO: new funtionen skal lave forbindelse til DB så den selv holder styr på sin egen forbindelse. du kan/skal bruge samme logik som
-    // Lars burge i DB.rs
-
-//TODO: lav en metode som er den logik Lars allerede har lavet i api 
 //let myrepo = pageRepository::new("sauron.db")
