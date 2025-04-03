c360fd3f12c3b5160f9013790350ce279d41e364
madsfjeldberg <mafj0001@stud.kea.dk>
2025-04-03 18:39:14 +0200

    change to only run cd on main branch

--- rust_rewrite/src/db.rs
+++ rust_rewrite/src/db.rs
@@ -1,7 +1,7 @@
 use tokio_rusqlite::{Connection, Result};
 
 pub async fn create_db_connection() -> Result<Connection> {
-  println!("->> Attempting to connect to database at /app/data/sauron.db");
+  println!("->> Attempting to connect to database at data/sauron.db");
   match Connection::open("data/sauron.db").await {
       Ok(conn) => {
           println!("->> Successfully connected to database");
