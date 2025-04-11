d9891bf1ff404251e995b4398a974cce7ef6be03
Lars <144362461+laha0006@users.noreply.github.com>
2025-04-11 19:56:40 +0200

    changed println to use new tracing info and error

--- rust_rewrite/src/db.rs
+++ rust_rewrite/src/db.rs
@@ -1,15 +1,15 @@
 use tokio_rusqlite::{Connection, Result};
-
+use tracing::{error, info};
 pub async fn create_db_connection() -> Result<Connection> {
-  println!("->> Attempting to connect to database at data/sauron.db");
-  match Connection::open("data/sauron.db").await {
-      Ok(conn) => {
-          println!("->> Successfully connected to database");
-          Ok(conn)
-      }
-      Err(e) => {
-          println!("->> Failed to connect to database: {:?}", e);
-          Err(e)
-      }
-  }
+    println!("->> Attempting to connect to database at data/sauron.db");
+    match Connection::open("data/sauron.db").await {
+        Ok(conn) => {
+            info!("->> Successfully connected to database");
+            Ok(conn)
+        }
+        Err(e) => {
+            error!("->> Failed to connect to database: {:?}", e);
+            Err(e)
+        }
+    }
 }
