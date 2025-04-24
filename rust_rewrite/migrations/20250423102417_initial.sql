346a659182a318dadebb4e778a4333450a035f36
madsfjeldberg <mafj0001@stud.kea.dk>
2025-04-24 11:21:25 +0200

    postgres working in docker-compose, table migrations working

--- rust_rewrite/migrations/20250423102417_initial.sql
+++ rust_rewrite/migrations/20250423102417_initial.sql
@@ -6,6 +6,16 @@ CREATE TABLE IF NOT EXISTS pages (
     content TEXT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
 -- Add indexes for better search performance
 CREATE INDEX IF NOT EXISTS idx_pages_language ON pages(language);
 CREATE INDEX IF NOT EXISTS idx_pages_content ON pages(content);
+CREATE INDEX IF NOT EXISTS idx_users_username ON users(username);
+CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
+CREATE INDEX IF NOT EXISTS idx_users_password ON users(password);
