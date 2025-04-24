bd11980d6ba8f02e032b0df169de5c5ae718a494
madsfjeldberg <mafj0001@stud.kea.dk>
2025-04-24 22:21:02 +0200

    register tests done.

--- rust_rewrite/tests/api_register_tests.rs
+++ rust_rewrite/tests/api_register_tests.rs
@@ -46,200 +46,128 @@ async fn test_register_success(pool: PgPool) {
     assert!(user_exists, "User was not found in database");
 }
 
-// #[tokio::test]
-// async fn test_register_invalid_email() {
-//     let db = Arc::new(Connection::open_in_memory().await.unwrap());
+#[sqlx::test]
+async fn test_register_invalid_email(pool: PgPool) {
 
-//     // Create the users table
+    let register_request = RegisterRequest {
+        username: "newuser".to_string(),
+        email: "invalid-email".to_string(), // Invalid email format
+        password: "password123".to_string(),
+    };
 
-//     let create_table = db
-//         .call(|conn| {
-//             conn.execute(
-//                 "CREATE TABLE users (
-//                     username TEXT,
-//                     email TEXT,
-//                     password TEXT
-//                 )",
-//                 [],
-//             )
-//             .map_err(|err| err.into())
-//         })
-//         .await;
-//     assert!(create_table.is_ok(), "Failed to create table");
+    let cookies = Cookies::default();
 
-//     let register_request = RegisterRequest {
-//         username: "newuser".to_string(),
-//         email: "invalid-email".to_string(), // Invalid email format
-//         password: "password123".to_string(),
-//     };
+    // Create a dummy header and body for the request.
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+    let body = Bytes::from(serde_json::to_string(&register_request).unwrap());
 
-//     let cookies = Cookies::default();
+    let response = api_register(State(Arc::new(pool)), cookies, headers, body)
+        .await
+        .into_response();
 
-//     // Create a dummy header and body for the request.
-//     let mut headers = HeaderMap::new();
-//     headers.insert("Content-Type", "application/json".parse().unwrap());
-//     let body = Bytes::from(serde_json::to_string(&register_request).unwrap());
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
 
-//     let response = api_register(State(db.clone()), cookies, headers, body)
-//         .await
-//         .into_response();
+#[sqlx::test]
+async fn test_register_username_exists(pool: PgPool) {
 
-//     assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
-// }
+    // Insert a test row
+    sqlx::query(
+        "INSERT INTO users (username, email, password)
+        VALUES ($1, $2, $3)",
+    )
+    .bind("existinguser")
+    .bind("test@example.com")
+    .bind("password123")
+    .execute(&pool)
+    .await
+    .expect("Failed to insert test data");
 
-// #[tokio::test]
-// async fn test_register_username_exists() {
-//     let db = Arc::new(Connection::open_in_memory().await.unwrap());
+    // Create a register request with the same username
+    let register_request = RegisterRequest {
+        username: "existinguser".to_string(),
+        email: "nottest@example.com".to_string(), // Different email so we know it's the username that's the issue.
+        password: "password123".to_string(),
+    };
 
-//     // Create the users table
-//     let create_table = db
-//         .call(|conn| {
-//             conn.execute(
-//                 "CREATE TABLE users (
-//                     username TEXT,
-//                     email TEXT,
-//                     password TEXT
-//                 )",
-//                 [],
-//             )
-//             .map_err(|err| err.into())
-//         })
-//         .await;
+    // Create a dummy header and body for the request.
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+    let body = Bytes::from(serde_json::to_string(&register_request).unwrap());
 
-//     assert!(create_table.is_ok(), "Failed to create table");
+    let cookies = Cookies::default();
 
-//     // Insert a test row
-//     let insert = db
-//         .call(move |conn| {
-//             conn.execute(
-//                 "INSERT INTO users (username, email, password)
-//                 VALUES (?1, ?2, ?3)",
-//                 params!["existinguser", "test@example.com", "password123"],
-//             )
-//             .map_err(|err| err.into())
-//         })
-//         .await;
-//     assert!(insert.is_ok(), "Failed to insert test data");
+    let response = api_register(State(Arc::new(pool)), cookies, headers, body)
+        .await
+        .into_response();
 
-//     let register_request = RegisterRequest {
-//         username: "existinguser".to_string(),
-//         email: "nottest@example.com".to_string(), // Different email so we know it's the username that's the issue.
-//         password: "password123".to_string(),
-//     };
+    assert_eq!(response.status(), StatusCode::CONFLICT);
+}
 
-//     // Create a dummy header and body for the request.
-//     let mut headers = HeaderMap::new();
-//     headers.insert("Content-Type", "application/json".parse().unwrap());
-//     let body = Bytes::from(serde_json::to_string(&register_request).unwrap());
+#[sqlx::test]
+async fn test_register_email_exists(pool: PgPool) {
 
-//     let cookies = Cookies::default();
+    // Insert a test row
+    sqlx::query(
+        "INSERT INTO users (username, email, password)
+        VALUES ($1, $2, $3)",
+    )
+    .bind("notexistinguser")
+    .bind("test@example.com")
+    .bind("password123")
+    .execute(&pool)
+    .await
+    .expect("Failed to insert test data");
 
-//     let response = api_register(State(db.clone()), cookies, headers, body)
-//         .await
-//         .into_response();
+    // Create a register request with the same email
+    let register_request = RegisterRequest {
+        username: "notexistinguser".to_string(), // Different username so we know it's the email that's the issue.
+        email: "test@example.com".to_string(),
+        password: "password123".to_string(),
+    };
 
-//     assert_eq!(response.status(), StatusCode::CONFLICT);
-// }
+    let cookies = Cookies::default();
 
-// #[tokio::test]
-// async fn test_register_email_exists() {
-//     let db = Arc::new(Connection::open_in_memory().await.unwrap());
+    // Create a dummy header and body for the request.
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+    let body = Bytes::from(serde_json::to_string(&register_request).unwrap());
 
-//     // Create the users table
-//     let create_table = db
-//         .call(|conn| {
-//             conn.execute(
-//                 "CREATE TABLE users (
-//                     username TEXT,
-//                     email TEXT,
-//                     password TEXT
-//                 )",
-//                 [],
-//             )
-//             .map_err(|err| err.into())
-//         })
-//         .await;
+    let response = api_register(State(Arc::new(pool)), cookies, headers, body)
+        .await
+        .into_response();
 
-//     assert!(create_table.is_ok(), "Failed to create table");
+    assert_eq!(response.status(), StatusCode::CONFLICT);
+}
 
-//     // Insert a test row
-//     let insert = db
-//         .call(move |conn| {
-//             conn.execute(
-//                 "INSERT INTO users (username, email, password)
-//                 VALUES (?1, ?2, ?3)",
-//                 params!["existinguser", "test@example.com", "password123"],
-//             )
-//             .map_err(|err| err.into())
-//         })
-//         .await;
-//     assert!(insert.is_ok(), "Failed to insert test data");
+#[sqlx::test]
+async fn test_register_url_encoded(pool: PgPool) {
 
-//     let register_request = RegisterRequest {
-//         username: "notexistinguser".to_string(), // Different username so we know it's the email that's the issue.
-//         email: "test@example.com".to_string(),
-//         password: "password123".to_string(),
-//     };
+    let register_request = RegisterRequest {
+        username: "testuser".to_string(),
+        email: "test@example.com".to_string(),
+        password: "password123".to_string(),
+    };
 
-//     let cookies = Cookies::default();
+    // Create a dummy header for the request.
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "Content-Type",
+        "application/x-www-form-urlencoded".parse().unwrap(),
+    );
 
-//     // Create a dummy header and body for the request.
-//     let mut headers = HeaderMap::new();
-//     headers.insert("Content-Type", "application/json".parse().unwrap());
-//     let body = Bytes::from(serde_json::to_string(&register_request).unwrap());
+    // Create the body as a url-encoded string.
+    let body = Bytes::from(format!(
+        "username={}&email={}&password={}",
+        register_request.username, register_request.email, register_request.password
+    ));
 
-//     let response = api_register(State(db.clone()), cookies, headers, body)
-//         .await
-//         .into_response();
+    let cookies = Cookies::default();
 
-//     assert_eq!(response.status(), StatusCode::CONFLICT);
-// }
+    let response = api_register(State(Arc::new(pool)), cookies, headers, body)
+        .await
+        .into_response();
 
-// #[tokio::test]
-// async fn test_register_url_encoded() {
-//     let db = Arc::new(Connection::open_in_memory().await.unwrap());
-
-//     // Create the users table
-//     let create_table = db
-//         .call(|conn| {
-//             conn.execute(
-//                 "CREATE TABLE users (
-//                     username TEXT,
-//                     email TEXT,
-//                     password TEXT
-//                 )",
-//                 [],
-//             )
-//             .map_err(|err| err.into())
-//         })
-//         .await;
-
-//     assert!(create_table.is_ok(), "Failed to create table");
-
-//     let register_request = RegisterRequest {
-//         username: "testuser".to_string(),
-//         email: "test@example.com".to_string(),
-//         password: "password123".to_string(),
-//     };
-
-//     // Create a dummy header for the request.
-//     let mut headers = HeaderMap::new();
-//     headers.insert(
-//         "Content-Type",
-//         "application/x-www-form-urlencoded".parse().unwrap(),
-//     );
-
-//     // Create the body as a url-encoded string.
-//     let body = Bytes::from(format!(
-//         "username={}&email={}&password={}",
-//         register_request.username, register_request.email, register_request.password
-//     ));
-
-//     let cookies = Cookies::default();
-
-//     let response = api_register(State(db.clone()), cookies, headers, body)
-//         .await
-//         .into_response();
-
-//     assert_eq!(response.status(), StatusCode::SEE_OTHER);
-// }
+    assert_eq!(response.status(), StatusCode::SEE_OTHER);
+}
